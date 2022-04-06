39ae6f9d1e9eeb3b0b045e9e57e2f3b5978f4489
JoaquinGA01 <otrebla.2817@gmail.com>
2022-04-06 07:18:11 -0500

    registrar usuario

--- app/Http/Controllers/Inicio.php
+++ app/Http/Controllers/Inicio.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+
 class Inicio extends Controller
 {
     //
@@ -13,6 +15,16 @@ class Inicio extends Controller
     }
 
     public function guardarUser(){
+        $conn = mysqli_connect("localhost", "root", "", "chein");
+        $nombre = $_POST['Name'];
+        $correo = $_POST['Email'];
+        $password = $_POST['Password'];
+        if($conn){
+            $sql = "INSERT INTO users(name, email, password) VALUES ('$nombre', '$correo', '$password')";
+            if (mysqli_query($conn, $sql)) {
+                
+            }
+        }
         return view('index');
     }
 }
